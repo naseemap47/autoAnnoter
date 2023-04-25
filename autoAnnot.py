ff69b3cae5e8e1f853ae42e3c09e7bec2f0eec62
NASEEM A P <naseemalassampattil@gmail.com>
2023-04-25 14:10:22 +0530

    added remove class option to ONNX model

--- autoAnnot.py
+++ autoAnnot.py
@@ -21,48 +21,33 @@ ap.add_argument("-s", "--size", type=int, required=True,
                 help="Size of image used to train the model")
 ap.add_argument("-conf", "--confidence", type=float, required=True,
                 help="Model detection Confidence (0<confidence<1)")
-
-
+ap.add_argument("-r", "--remove", nargs='+', default=[],
+                help="List of classes need to remove")
 args = vars(ap.parse_args())
-XML = args['xml']
-TXT = args['txt']
-path_to_dir = args["dataset"]
-path_to_txt = args['classes']
-onnx_model_path = args['model']
-img_size = args['size']
-detect_conf = args['confidence']
+
 
 # ONNX Model
-onnx_session = onnxruntime.InferenceSession(onnx_model_path)
+onnx_session = onnxruntime.InferenceSession(args['model'])
 
-img_list = glob.glob(os.path.join(path_to_dir, '*.jpg')) + \
-    glob.glob(os.path.join(path_to_dir, '*.jpeg')) + \
-    glob.glob(os.path.join(path_to_dir, '*.png'))
+img_list = glob.glob(os.path.join(args["dataset"], '*.jpg')) + \
+    glob.glob(os.path.join(args["dataset"], '*.jpeg')) + \
+    glob.glob(os.path.join(args["dataset"], '*.png'))
+class_names = read_txt_lines(args['classes'])
 
-# XML Annotation
-if XML:
-    for img in img_list:
-        image = cv2.imread(img)
-        h, w, c = image.shape
-        bbox_list, class_list, confidence = findBBox(
-            onnx_session, image, img_size, detect_conf)
-        folder_name, file_name = os.path.split(img)
-        class_names = read_txt_lines(path_to_txt)
+for img in img_list:
+    image = cv2.imread(img)
+    h, w, c = image.shape
+    bbox_list, class_list, confidence = findBBox(
+        onnx_session, image, args['size'], args['confidence'], class_names, args['remove'])
+    folder_name, file_name = os.path.split(img)
+    # XML Annotation
+    if args['xml']:
         save_xml(folder_name, file_name, img, w, h, c,
                  bbox_list, class_list, class_names)
         print(f'Successfully Annotated {file_name}')
 
-    print('XML-Auto_Annotation Successfully Completed')
-
-# YOLO Annotation
-if TXT:
-    for img in img_list:
-        image = cv2.imread(img)
-        h, w, c = image.shape
-        bbox_list, class_list, confidence = findBBox(
-            onnx_session, image, img_size, detect_conf)
-        folder_name, file_name = os.path.split(img)
+    # YOLO Annotation
+    if args['txt']:
         save_yolo(folder_name, file_name, w, h, bbox_list, class_list)
         print(f'Successfully Annotated {file_name}')
-
-    print('TXT-Auto_Annotation Successfully Completed')
+print(f"{'TXT' if args['txt'] else 'XML'}-Auto_Annotation Successfully Completed")
