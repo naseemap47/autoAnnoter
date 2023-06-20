75d79e6f8a6153c4e8228d213a30e482f15989ca
NASEEM A P <naseemalassampattil@gmail.com>
2023-06-20 15:25:06 +0530

    added keep args to detectio fn

--- anot_utils.py
+++ anot_utils.py
@@ -27,7 +27,7 @@ def remove_class(class_list, id, remove_list):
     return name in set(remove_list)
 
 # to get bounding box from ONNX model
-def findBBox(onnx_session, img, img_resize, threshold, class_name_list, remove_list):
+def findBBox(onnx_session, img, img_resize, threshold, class_name_list, remove_list, keep_list):
   # onnx session
     input_name = onnx_session.get_inputs()[0].name
 
@@ -45,6 +45,23 @@ def findBBox(onnx_session, img, img_resize, threshold, class_name_list, remove_l
     for i in ort_outs[4][0]:
         if i > threshold:
             if not remove_class(class_name_list, int(ort_outs[2][0][c]), remove_list):
+                bbox = ort_outs[1][0][c]
+                ymin = (bbox[0])
+                xmin = (bbox[1])
+                ymax = (bbox[2])
+                xmax = (bbox[3])
+                # cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                xmin, ymin, xmax, ymax = int(
+                    xmin*w), int(ymin*h), int(xmax*w), int(ymax*h)
+                bbox_list.append([xmin, ymin, xmax, ymax])
+
+                # Detection Classes
+                class_list.append(ort_outs[2][0][c])
+
+                # confidence
+                confidence.append(i)
+            
+            if remove_class(class_name_list, int(ort_outs[2][0][c]), keep_list):
                 bbox = ort_outs[1][0][c]
                 ymin = (bbox[0])
                 xmin = (bbox[1])
@@ -158,7 +175,7 @@ def save_yolo(folder_name, file_name, w, h, bbox_list, class_list):
 
 
 # YOLOv7
-def get_BBoxYOLOv7(img, yolo_model, detect_conf, class_name_list, remove_list):
+def get_BBoxYOLOv7(img, yolo_model, detect_conf, class_name_list, remove_list, keep_list):
 
     # Load YOLOv7 model on Image
     results = yolo_model(img)
@@ -187,11 +204,21 @@ def get_BBoxYOLOv7(img, yolo_model, detect_conf, class_name_list, remove_list):
                 class_ids.append(id)
                 # Confidence
                 confidence.append(conf)
+
+            # Keep specfic classes from Annotation
+            if remove_class(class_name_list, int(id-1), keep_list):
+                # BBox
+                bbox_list.append([xmin, ymin, xmax, ymax])
+                # class
+                class_ids.append(id)
+                # Confidence
+                confidence.append(conf)
+
     return bbox_list, class_ids, confidence
 
 
 # YOLOv8
-def get_BBoxYOLOv8(img, yolo_model, detect_conf, class_name_list, remove_list):
+def get_BBoxYOLOv8(img, yolo_model, detect_conf, class_name_list, remove_list, keep_list):
 
     bbox_list = []
     confidence = []
@@ -221,11 +248,20 @@ def get_BBoxYOLOv8(img, yolo_model, detect_conf, class_name_list, remove_list):
                     # Confidence
                     confidence.append(cnf)
 
+                # Keep specfic classes from Annotation
+                if remove_class(class_name_list, int(id-1), keep_list):
+                    # BBox
+                    bbox_list.append([xmin, ymin, xmax, ymax])
+                    # class
+                    class_ids.append(id)
+                    # Confidence
+                    confidence.append(conf)
+
     return bbox_list, class_ids, confidence
 
 
 # YOLOv8
-def get_BBoxYOLONAS(img, yolo_model, detect_conf, class_name_list, remove_list):
+def get_BBoxYOLONAS(img, yolo_model, detect_conf, class_name_list, remove_list, keep_list):
 
     bbox_list = []
     confidence = []
@@ -242,6 +278,15 @@ def get_BBoxYOLONAS(img, yolo_model, detect_conf, class_name_list, remove_list):
         if cnf > detect_conf:
             # Remove specfic classes from Annotation
             if not remove_class(class_name_list, int(cs), remove_list):
+                # BBox
+                bbox_list.append(box[:4])
+                # class
+                class_ids.append(int(cs+1))
+                # Confidence
+                confidence.append(cnf)
+
+            # Keep specfic classes from Annotation
+            if remove_class(class_name_list, int(id-1), keep_list):
                 # BBox
                 bbox_list.append(box[:4])
                 # class
