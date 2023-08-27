9563f08e58c3e90b5a2206566e535207d4418c3b
NASEEM A P <naseemalassampattil@gmail.com>
2023-08-27 10:14:35 +0530

    added create_yolo - to convert mixed images and labels into images and labels dirs

---
+++ tools/create_yolo.py
@@ -0,0 +1,48 @@
+import glob
+import shutil
+import argparse
+import os
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--data", type=str, required=True,
+                help="path to image/dir/data")
+args = vars(ap.parse_args())
+
+
+def move_file(f_list, dst):
+    for f in f_list:
+        shutil.move(f, dst)
+        print(f"[INFO] Successfully Moved {os.path.split(f)[1]}")
+
+
+img_list = glob.glob(os.path.join(args["data"], '*.jpg')) + \
+    glob.glob(os.path.join(args["data"], '*.jpeg')) + \
+    glob.glob(os.path.join(args["data"], '*.png'))
+
+if len(img_list)>0:
+    os.makedirs('images', exist_ok=True)
+
+txt_list = glob.glob(args["data"], '*.txt')
+xml_list = glob.glob(args["data"], '*.xml')
+
+if len(txt_list)>0 and len(xml_list) == 0:
+    os.makedirs('labels', exist_ok=True)
+    move_file(img_list, os.path.join(args["data"], 'images'))
+    move_file(txt_list, os.path.join(args["data"], 'labels'))
+    
+elif len(txt_list) == 0 and len(xml_list)>0:
+    os.makedirs('labels', exist_ok=True)
+    move_file(img_list, os.path.join(args["data"], 'images'))
+    move_file(xml_list, os.path.join(args["data"], 'labels'))
+
+elif len(txt_list)>0 and len(xml_list)>0:
+    os.makedirs('txt_labels', exist_ok=True)
+    os.makedirs('xml_labels', exist_ok=True)
+    move_file(img_list, os.path.join(args["data"], 'images'))
+    move_file(txt_list, os.path.join(args["data"], 'txt_labels'))
+    move_file(xml_list, os.path.join(args["data"], 'xml_labels'))
+    
+else:
+    print('[INFO] Labels NOT Found!!!')
+
