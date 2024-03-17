8068a8f6211b15ada17296cdcf02290726994e8b
NASEEM A P <naseemalassampattil@gmail.com>
2024-03-17 14:19:14 +0530

    updated txt aug with respect to aug yaml file

--- Augmentation/txt_aug.py
+++ Augmentation/txt_aug.py
@@ -1,12 +1,8 @@
-from data_aug import RandomHorizontalFlip, HorizontalFlip, RandomScale, Scale, RandomTranslate, Translate,\
-                    RandomRotate, Rotate, RandomShear, Shear, Resize, RandomHSV, Sequence
+from Augmentation.aug_utils import *
+from numpy.random import choice
 import glob
 import os
-import cv2
 import argparse
-from tool_utils import get_txt, write_txt
-import numpy as np
-from bbox_util import rotate_im
 import yaml
 
 
@@ -14,56 +10,94 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, required=True,
                 help="path to image/dir")
 ap.add_argument("-t", "--txt", type=str, required=True,
-                help="path to txt/dir")
+                help="path to (YOLO) txt/dir")
 ap.add_argument("-s", "--save", type=str, required=True,
-                help="path to save")
-
+                help="path to save aug yolo (txt)")
+ap.add_argument("-y", "--yaml", type=str, default='Augmentation/default.yaml',
+                help="path to aug yaml file")
 args = vars(ap.parse_args())
-path_to_img = args["image"]
-path_to_txt = args['txt']
-path_to_save = args['save']
-os.makedirs(path_to_save, exist_ok=True)
-os.makedirs(f"{path_to_save}/images", exist_ok=True)
-os.makedirs(f"{path_to_save}/labels", exist_ok=True)
 
-txt_list = sorted(glob.glob(f'{path_to_txt}/*.txt'))
-img_full_list = glob.glob(f'{path_to_img}/*.jpeg') + \
-                glob.glob(f'{path_to_img}/*.jpg')  + \
-                glob.glob(f'{path_to_img}/*.png')
 
+with open(args['yaml'], 'r') as f:
+	data = yaml.load(f, Loader=yaml.SafeLoader)
+print('Augmentation Params:\n', data)
+
+os.makedirs(args['save'], exist_ok=True)
+img_full_list = glob.glob(f"{args['image']}/*.jpeg") + \
+                glob.glob(f"{args['image']}/*.jpg")  + \
+                glob.glob(f"{args['image']}/*.png")
 img_list = sorted(img_full_list)
-# img_aug = np.zeros((2, 2, 3))
-# img_aug = None
-c = 0
-for txt_path, img_path in zip(txt_list, img_list):
-    c += 1
-    # Locations
-    folder_name, img_name = os.path.split(img_path)
-    bbox_list, class_list = get_txt(txt_path, img_path)
-    img = cv2.imread(img_path)
-    
-    if len(bbox_list) > 0:
-        # Augumentation
-        bbox_list = np.array(bbox_list, dtype=np.float64)
-        # if c%3 == 0:
-        img_aug, bbox_aug = Rotate(180)(img.copy(), bbox_list.copy())
-    else:
-        # img_aug = img[:, ::-1, :]
-        img_aug = rotate_im(img, 180)
-        bbox_aug = []
 
-        # elif c%4 == 0:
-        #     img_aug, bbox_aug = RandomScale(0.2, diff = True)(img.copy(), bbox_list.copy())
-        # elif c%5 == 0:
-        #     img_aug, bbox_aug = RandomRotate(20)(img.copy(), bbox_list.copy())
-        # elif c%6 == 0:
-        #     img_aug, bbox_aug = RandomShear(0.2)(img.copy(), bbox_list.copy())
-        # else:
+# image HSV-Hue augmentation
+total_prob = int((data['hsv_h']['prob'])*len(img_list))
+hsv_h_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomHSV(hsv_h_img, args['txt'], hue=data['hsv_h']['hue'], path_to_save=args['save'], name_id='hsv_h')
 
-        # Write Aug
-        # if len(bbox_aug)>0:
-    h, w, _ = img_aug.shape
-    cv2.imwrite(f"{path_to_save}/images/{img_name}", img_aug)
-    anot_name = f"{path_to_save}/labels/{os.path.splitext(img_name)[0]}.txt"
-    write_txt(anot_name, bbox_aug, class_list, h, w)
-    print(f"[INFO] Saved {anot_name}")
+# image HSV-Saturation augmentation
+total_prob = int((data['hsv_s']['prob'])*len(img_list))
+hsv_s_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomHSV(hsv_s_img, args['txt'], saturation=data['hsv_s']['saturation'], path_to_save=args['save'], name_id='hsv_s')
+
+# image HSV-Value (brightness) augmentation
+total_prob = int((data['hsv_v']['prob'])*len(img_list))
+hsv_v_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomHSV(hsv_v_img, args['txt'], brightness=data['hsv_v']['brightness'], path_to_save=args['save'], name_id='hsv_v')
+
+# Mixed image HSV augmentation (Mixed HSV-Hue, HSV-Saturation and HSV-Value (brightness))
+total_prob = int((data['hsv']['prob'])*len(img_list))
+hsv_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomHSV(
+    hsv_img, args['txt'], hue=data['hsv']['hue'], saturation=data['hsv']['saturation'], 
+    brightness=data['hsv']['brightness'], path_to_save=args['save'], name_id='hsv'
+)
+
+# image rotation augmentation
+total_prob = int((data['degrees']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_Rotate(rot_img, args['txt'], args['save'], 'rot', data['degrees']['deg'])
+
+# image Random rotation augmentation
+total_prob = int((data['degrees_random']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomRotate(rot_img, args['txt'], args['save'], 'rot_random', data['degrees_random']['deg'])
+
+# image Translate augmentation
+total_prob = int((data['translate']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_Translate(rot_img, args['txt'], args['save'], 'trans', data['translate']['translate_x'], data['translate']['translate_y'])
+
+# image Random Translate augmentation
+total_prob = int((data['translate_random']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomTranslate(rot_img, args['txt'], args['save'], 'trans_random', data['translate_random']['translate'])
+
+# image Scale augmentation
+total_prob = int((data['scale']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_Scale(rot_img, args['txt'], args['save'], 'scale', data['scale']['scale_x'], data['scale']['scale_y'])
+
+# image Random Scale augmentation
+total_prob = int((data['scale_random']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomScale(rot_img, args['txt'], args['save'], 'scale_random', data['scale_random']['scale'])
+
+# image Shear augmentation
+total_prob = int((data['shear']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_Shear(rot_img, args['txt'], args['save'], 'shear', data['shear']['shear'])
+
+# image Random Shear augmentation
+total_prob = int((data['shear_random']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_RandomShear(rot_img, args['txt'], args['save'], 'shear_random', data['shear_random']['shear'])
+
+# image flip up-down augmentation
+total_prob = int((data['flipud'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_flipud(rot_img, args['txt'], args['save'], 'flipud')
+
+# image flip left-right augmentation
+total_prob = int((data['fliplr'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anotT_fliplr(rot_img, args['txt'], args['save'], 'fliplr')
+
