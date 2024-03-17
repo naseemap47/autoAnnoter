d60537e3d40261672a7cfd8544fde3897de706c3
NASEEM A P <naseemalassampattil@gmail.com>
2024-03-17 12:36:09 +0530

    added Random translate augmentation

--- tools/xml_aug.py
+++ tools/xml_aug.py
@@ -95,6 +95,27 @@ def generate_anot_Translate(image_list:list, path_to_labels:str, path_to_save:st
                 write_xml(img_path, bbox_aug, class_list, path_to_xml_save)
 
 
+def generate_anot_RandomTranslate(image_list:list, path_to_labels:str, path_to_save:str, name_id:str, translate:float):
+    for img_path in image_list:
+        img_name = os.path.split(img_path)[1]
+        img = cv2.imread(img_path)
+
+        # Read XML
+        xml_path = f"{path_to_labels}/{os.path.splitext(img_name)[0]}.xml"
+        bbox_list, class_list = get_xml(xml_path)
+        if len(bbox_list) > 0:
+            # Augumentation
+            bbox_list = np.array(bbox_list, dtype=np.float64)
+            img_aug, bbox_aug = RandomTranslate(translate)(img.copy(), bbox_list.copy())
+            if len(bbox_aug) > 0:
+                # save image
+                cv2.imwrite(f"{path_to_save}/{os.path.splitext(img_name)[0]}_{name_id}.jpg", img_aug)
+                # Save XML
+                xml_name = f"{os.path.splitext(img_name)[0]}_{name_id}.xml"
+                path_to_xml_save = f"{path_to_save}/{xml_name}"
+                write_xml(img_path, bbox_aug, class_list, path_to_xml_save)
+
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, required=True,
                 help="path to image/dir")
@@ -153,4 +174,9 @@ generate_anot_RandomRotate(rot_img, path_to_xml, path_to_save, 'rot_random', dat
 total_prob = int((data['translate']['prob'])*len(img_list))
 rot_img = choice(img_list, total_prob, replace=False)
 generate_anot_Translate(rot_img, path_to_xml, path_to_save, 'trans', data['translate']['translate_x'], data['translate']['translate_y'])
+
+# image Random Translate augmentation
+total_prob = int((data['translate_random']['prob'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anot_Translate(rot_img, path_to_xml, path_to_save, 'trans', data['translate_random']['translate'])
 
