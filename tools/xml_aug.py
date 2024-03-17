e4225fc7d0f7054c986e13552c6b8a9616f801d4
NASEEM A P <naseemalassampattil@gmail.com>
2024-03-17 12:58:24 +0530

    added flip up-down augmentation

--- tools/xml_aug.py
+++ tools/xml_aug.py
@@ -200,6 +200,27 @@ def generate_anot_RandomShear(image_list:list, path_to_labels:str, path_to_save:
                 write_xml(img_path, bbox_aug, class_list, path_to_xml_save)
 
 
+def generate_anot_flipud(image_list:list, path_to_labels:str, path_to_save:str, name_id:str):
+    for img_path in image_list:
+        img_name = os.path.split(img_path)[1]
+        img = cv2.imread(img_path)
+
+        # Read XML
+        xml_path = f"{path_to_labels}/{os.path.splitext(img_name)[0]}.xml"
+        bbox_list, class_list = get_xml(xml_path)
+        if len(bbox_list) > 0:
+            # Augumentation
+            bbox_list = np.array(bbox_list, dtype=np.float64)
+            img_aug, bbox_aug = Rotate(180)(img.copy(), bbox_list.copy())
+            if len(bbox_aug) > 0:
+                # save image
+                cv2.imwrite(f"{path_to_save}/{os.path.splitext(img_name)[0]}_{name_id}.jpg", img_aug)
+                # Save XML
+                xml_name = f"{os.path.splitext(img_name)[0]}_{name_id}.xml"
+                path_to_xml_save = f"{path_to_save}/{xml_name}"
+                write_xml(img_path, bbox_aug, class_list, path_to_xml_save)
+
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, required=True,
                 help="path to image/dir")
@@ -283,4 +304,9 @@ generate_anot_Shear(rot_img, path_to_xml, path_to_save, 'shear', data['shear']['
 total_prob = int((data['shear_random']['prob'])*len(img_list))
 rot_img = choice(img_list, total_prob, replace=False)
 generate_anot_RandomShear(rot_img, path_to_xml, path_to_save, 'shear_random', data['shear_random']['shear'])
+
+# image flip up-down augmentation
+total_prob = int((data['flipud'])*len(img_list))
+rot_img = choice(img_list, total_prob, replace=False)
+generate_anot_flipud(rot_img, path_to_xml, path_to_save, 'flipud')
 
